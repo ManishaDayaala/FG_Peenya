cb6cbd835724eab8234566ccbe86f7f1b38452e3
ManishaDayaala <Manishadayaala@gmail.com>
2025-06-24 17:19:08 +0530

    Update FG_peenya.py

--- FG_peenya.py
+++ FG_peenya.py
@@ -645,6 +645,11 @@ else:
         # Load test and threshold data
         test_df = pd.read_excel(test_file_path)
         threshold_df = pd.read_excel(threshold_file_path)
+        # Show available columns in the dataset
+        with st.expander("📊 Show available columns in test data"):
+              st.write("Below are the column names found in the test data file:")
+              st.code("\n".join(test_df.columns), language="text")
+
 
         if test_df.empty:
             st.warning("NO DATA in the test file.")
